b04e97c0931d1b4e710cd1b308db2f2081aaefcf
Burak Cabas <burakcabas3@gmail.com>
2026-02-07 14:07:16 +0300

    update

--- src/main/java/example/OnlyMain.java
+++ src/main/java/example/OnlyMain.java
@@ -1,26 +1,9 @@
 package example;
 
-import java.util.Collections;
-import java.util.LinkedList;
-
 public class OnlyMain {
 
     public static void main(String[] args){
 
-        LinkedList<String> hairstyles = new LinkedList<String>();
-
-        hairstyles.add("curly");
-        hairstyles.add("straight");
-        hairstyles.add("bald");
-
-        System.out.println(hairstyles);
-
-        Collections.sort(hairstyles);
-
-        System.out.println(hairstyles);
-
-        Collections.sort(hairstyles, Collections.reverseOrder());
-        System.out.println(hairstyles);
 
 
     }
